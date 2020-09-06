5e9cd298044ea3d5eedc046d672022e4d5f9ad5d
aneeshkandal <70319972+aneeshkandal@users.noreply.github.com>
2020-09-06 20:00:22 +0530

    Assignment

---
+++ app.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict'
+
+  angular.module('NameCheck', [])
+  .controller('NameCheckController', NameCheckController);
+
+  NameCheckController.$inject = ['$scope'];
+
+  function NameCheckController($scope) {
+    $scope.result='';
+    $scope.Names='';
+
+  $scope.NameCheck = function () {
+    if($scope.Names.trim().length == 0) {
+      $scope.isEmpty = true;
+      }
+    else {
+      $scope.isEmpty = false;
+
+      var splitNames = $scope.Names.split(',');
+      var checkName = splitNames.filter(function(Name){
+        return Name.trim() != '';
+      })
+
+      if(checkName.length <= 3 && checkName.length >= 3 ) {
+        $scope.result = "Group Of 3 registered";
+      }
+      else {
+        $scope.result = "Error - Must enter 3 names";
+      }
+
+      }
+    };
+  }
+})(); 
